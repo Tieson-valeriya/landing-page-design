542bd6a642a23a28733cc8e5c4048e3dfcaf91a6
tieson-valeriya <80436249+tieson-valeriya@users.noreply.github.com>
2021-04-14 13:35:02 +0900

    source code

---
+++ assets/javascript.js
@@ -0,0 +1,25 @@
+!(function($) {
+  "use strict";
+  // type effect
+  if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+    var typed = new Typed('.text-slider', {
+      strings: typed_strings.split(','),
+      typeSpeed: 120,
+      loop: true,
+      backDelay: 1100,
+      backSpeed: 50
+    });
+  }
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+})(jQuery);
